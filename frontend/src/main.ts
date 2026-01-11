f87e8bc3afb22595979ec078d13f14e7f7f96eaa
Elian-beep <elian.19batista@gmail.com>
2026-01-11 11:44:55 -0400

    chore: [front] config vue query global

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,21 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
 
-const app = createApp(App)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+      retry: 2,
+    },
+  },
+});
 
-app.use(createPinia())
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(VueQueryPlugin, { queryClient });
+
+app.mount('#app');
